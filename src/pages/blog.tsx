d10e0f2991d0265b55c50618762c14f982e10b48
dediirawanuti <dedi_irawan@teknokrat.ac.id>
2023-09-04 18:01:37 +0700

    commit firts

---
+++ src/pages/blog.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Blog() {
+    return (
+        <>
+            <main>
+                <div className="flex flex-col">
+                    <div className="card">
+                        <div className="bg-gray-800 h-[80px] w-full">
+                            <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
